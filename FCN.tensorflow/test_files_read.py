74f04ca71ab2b6847e0df252a5fa3b5d408a4f66
laughing.ye <laughing.ye@corp.ro8ro.com>
2019-07-06 12:57:41 +0800

    new

---
+++ FCN.tensorflow/test_files_read.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'laughing.ye@corp.to8to.com'
+
+
+__author__ = 'charlie'
+import numpy as np
+import os
+import random
+from six.moves import cPickle as pickle
+from tensorflow.python.platform import gfile
+import glob
+
+import TensorflowUtils as utils
+
+# DATA_URL = 'http://sceneparsing.csail.mit.edu/data/ADEChallengeData2016.zip'
+DATA_URL = 'http://data.csail.mit.edu/places/ADEchallenge/ADEChallengeData2016.zip'
+
+
+def read_dataset(data_dir):
+    pickle_filename = "test.pickle"
+    pickle_filepath = os.path.join(data_dir, pickle_filename)
+    if not os.path.exists(pickle_filepath):
+        SceneParsing_folder = 'test'
+        result = create_image_lists(os.path.join(data_dir, SceneParsing_folder))
+        print ("Pickling ...")
+        with open(pickle_filepath, 'wb') as f:
+            pickle.dump(result, f, pickle.HIGHEST_PROTOCOL)
+    else:
+        print ("Found pickle file!")
+
+    with open(pickle_filepath, 'rb') as f:
+        result = pickle.load(f)
+        training_records = result['training']
+        del result
+
+    return training_records
+
+
+def create_image_lists(image_dir):
+    if not gfile.Exists(image_dir):
+        print("Image directory '" + image_dir + "' not found.")
+        return None
+    directories = ['test']
+    image_list = {}
+
+    for directory in directories:
+        file_list = []
+        image_list[directory] = []
+        file_glob = os.path.join(image_dir, "images", directory, '*.' + 'jpg')
+        file_list.extend(glob.glob(file_glob))
+
+        if not file_list:
+            print('No files found')
+        else:
+            for f in file_list:
+                filename = os.path.splitext(f.split("\\")[-1])[0]
+                annotation_file = os.path.join(image_dir, "annotations", directory, filename + '.png')
+                if os.path.exists(annotation_file):
+                    record = {'image': f, 'annotation': annotation_file, 'filename': filename}
+                    image_list[directory].append(record)
+                else:
+                    print("Annotation file not found for %s - Skipping" % filename)
+
+        random.shuffle(image_list[directory])
+        no_of_images = len(image_list[directory])
+        print ('No. of %s files: %d' % (directory, no_of_images))
+
+    return image_list
